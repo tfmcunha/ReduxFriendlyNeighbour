a9476121a36eb0aabaed07eb73c25ee5155df08f
Tiago Cunha <tcunha_lp@hotmail.com>
2019-08-10 22:51:33 +0100

    refactoring with redux

--- src/components/loginform.js
+++ src/components/loginform.js
@@ -1,131 +1,117 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
 import { API_ROOT } from '../constants';
 import { Redirect } from 'react-router-dom';
 import { Form, Button } from 'react-bootstrap';
+
 import Auth from '../modules/auth';
 
+function LoginForm(props){
 
-class LoginForm extends Component {
-	constructor() {
-		super();
-		this.state = {
-			email: "",
-			password: "",
-			form_errors: {},
-			errors: {}
-		}
-		this.handleChange=this.handleChange.bind(this);	
-		this.handleLogin=this.handleLogin.bind(this);	
-	}
+	const [ email, setEmail ] = useState("");
+	const [ password, setPassword ] = useState("");
+	const [ formErrors, setFormErrors ] = useState({});
 
-	handleAuthentication() {
-		this.setState({
-			auth: Auth.isUserAuthenticated()
-		})
-	}  
-
-	handleChange(e) {
-		const name = e.target.name;
-     	const value = e.target.value;
-    	this.setState({ 
-        	[name]: value,
-		})
-	}
-
-	validateForm() {
-		const email = this.state.email;
-		const password = this.state.password;		
-		let form_errors = {};
-		let formIsValid = true;
+	function validateForm() {
+		let formIsValid = true
 
 		if (!email) {
 			formIsValid = false;
-			form_errors["email"] = "*Please enter your email";
+			setFormErrors({
+				...formErrors,
+				email: "*Please enter your email"
+			})			
 		} 
 
 		if (typeof email !== "undefined") {      
 			var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
 			if (!pattern.test(email)) {
 				formIsValid = false;
-				form_errors["email"] = "*Please enter valid email.";
+				setFormErrors({
+					...formErrors,
+					email: "*Please enter valid email."
+				})				
 			}
 		}
 
 		if (!password) {
 			formIsValid = false;
-			form_errors["password"] = "*Please enter your password";
+			setFormErrors({
+				...formErrors,
+				password: "*Please enter your password"
+			})	
 		}
 
-		this.setState({
-			form_errors
-		})
-
 		return formIsValid;
-
 	}
 
-	handleLogin(e) {
+	function handleLogin(e) {
 		e.preventDefault();
 		const loginData = {}
-		loginData["email"] = this.state.email
-		loginData["password"] = this.state.password
-		if ( this.validateForm() ) {		    
-		    fetch(`${API_ROOT}/login`, { 
-		      method: 'POST', 
-		      body: JSON.stringify(loginData), 
-		      headers: {
-		        'Content-Type': 'application/json'
-		      }
-		    })
-		    .then(res => res.json())
-		    .then(json => {
-		    	if (json.token !== undefined) {
-			    	Auth.authenticateToken(json.token);		    	
-			    	this.handleAuthentication();
-			    } else {
-			    	this.setState({
-			    		errors: json
-			    	})
-			    }		    
-		    })
+		loginData["email"] = email
+		loginData["password"] = password
+		if ( validateForm() ) {		    
+		    props.setAuthentication(loginData)		    
 		}	       
-	}
-
-
+	}	
 	
-
- 	render() {
- 		const errors = this.state.errors.errors; 		
-		
-    	return (
-      		<div className="p-4">
-      			{Auth.isUserAuthenticated() &&
-      				<Redirect to="/dashboard" />
-      			}
-	      			<h3 className="text-center">LOGIN</h3>
-	      			<Form onSubmit={this.handleLogin}>
-	      				<Form.Group>
-	      					<Form.Label>E-mail</Form.Label>
-			      			<Form.Control type="text" name="email" value={this.state.email} onChange={this.handleChange} autoComplete="off"/>
-			      			<Form.Text className="text-danger">{this.state.form_errors.email}</Form.Text>
-						</Form.Group>		      			
-			      		<Form.Group>
-			      			<Form.Label>Password</Form.Label>
-			      			<Form.Control type="password" name="password" value={this.state.password} onChange={this.handleChange} />
-			      			<Form.Text className="text-danger">{this.state.form_errors.password}</Form.Text>
-			      		</Form.Group>
-			      		{errors !== undefined &&
-	      				errors.map(error => (
-	      					<Form.Group>
-	      						<Form.Text className="text-danger">*{error.detail}</Form.Text>
-	      					</Form.Group>
-	      				))
-	      				}
-			      		<Button variant="primary" type="submit" >OK</Button>		      		
-	      			</Form>      			
-      		</div>      				
-    	);
-  	}
+	return (
+  		<div className="p-4">
+  			{Auth.isUserAuthenticated() &&
+  				<Redirect to="/dashboard" />
+  			}
+      			<h3 className="text-center">LOGIN</h3>
+      			<Form onSubmit={handleLogin}>
+      				<Form.Group>
+      					<Form.Label>E-mail</Form.Label>
+		      			<Form.Control type="text" name="email" value={email} onChange={(e) => setEmail(e.target.value)} autoComplete="off"/>
+		      			<Form.Text className="text-danger">{formErrors.email}</Form.Text>
+					</Form.Group>		      			
+		      		<Form.Group>
+		      			<Form.Label>Password</Form.Label>
+		      			<Form.Control type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+		      			<Form.Text className="text-danger">{formErrors.password}</Form.Text>
+		      		</Form.Group>
+		      		{props.errors !== undefined &&
+      					props.errors.map(error => (
+      						<Form.Group>
+      							<Form.Text className="text-danger">*{error.detail}</Form.Text>
+      						</Form.Group>
+      				))
+      				}
+		      		<Button variant="primary" type="submit" >OK</Button>		      		
+      			</Form>      			
+  		</div>      				
+	);  			
 }
 
-export default LoginForm;
+function mapStateToProps(state) {
+	return{
+		auth: state.authenticated,
+		errors: state.errors
+	}		
+}
+
+function mapDispatchToProps(dispatch) {
+	return {
+		setAuthentication: (loginData) => {
+			dispatch(async () => {
+				const res = await fetch(`${API_ROOT}/login`, { 
+		    	  	method: 'POST', 
+		      		body: JSON.stringify(loginData), 
+		      		headers: {
+		      		  	'Content-Type': 'application/json'
+		      		}
+		    	})
+		    	const data = await res.json()
+		    	if(data.token) {
+		    		dispatch({type: "SET_AUTHENTICATION", payload:data.token})	
+		    	} else {
+		    		dispatch({type: "SET_AUTHENTICATION_ERRORS", payload:data.errors})			
+		    	}				
+			}).catch(e => console.log(e))				
+		}
+	}
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(LoginForm)
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,18 +1,24 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk'
+import Auth from '../modules/auth';
 
 const initialState = {
-
+	authenticated: Auth.isUserAuthenticated(),
+	errors: [] 
 }
 
 const reducer = (state = initialState, action) => {
 	switch (action.type) {
-		case "label_1":
-			return state
+		case "SET_AUTHENTICATION":
+			Auth.authenticateToken(action.payload)			
+			return {...state, authenticated: Auth.isUserAuthenticated() }
+		case "SET_AUTHENTICATION_ERRORS":			
+			return {...state, errors: action.payload }
 		default:
 			return state
 	}
 }
 
-const store = createStore(reducer)
+const store = createStore(reducer, applyMiddleware(thunk))
 
 export default store
